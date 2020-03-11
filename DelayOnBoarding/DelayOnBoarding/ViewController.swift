f98402e84e7ce599f366171c48185d52012bd51a
mohammed <mdrame113@gmail.com>
2020-03-11 13:26:52 -0700

    restarting project

---
+++ DelayOnBoarding/DelayOnBoarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelayOnBoarding
+//
+//  Created by Mohammed Drame on 3/11/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
